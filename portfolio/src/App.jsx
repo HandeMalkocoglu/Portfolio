f246791a5ec4a46474227d53bd060d6b01c23fdf
HandeMalkocoglu <handemalkocoglu@hotmail.com>
2025-02-05 19:35:37 +0300

    Tema ve Dil Seçeneği eklendi

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "../src/components/Header";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
